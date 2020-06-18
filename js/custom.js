bddb343953171b52e6b35e55e2b316be4e759efe
MD JAHIR UDDIN SHOHAN <66644790+shohan-utd@users.noreply.github.com>
2020-06-18 21:21:08 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$('#case_carousel_active').owlCarousel({
+	items:3,
+	loop:true,
+	margin:30,
+	dots:false,
+	nav:false,
+});
+
+$('#testmonial_car_active').owlCarousel({
+	items:1,
+	loop:true,
+	dots:false,
+	nav:false,
+});
+
+$('#logo_car_active').owlCarousel({
+	items:6,
+	loop:true,
+	dots:false,
+	nav:false,
+});
